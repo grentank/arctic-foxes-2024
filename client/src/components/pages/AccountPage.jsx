a482f6bb9f5360c3649b36655075b04c1d30e0f2
grentank <mexmat100394@gmail.com>
2024-04-29 00:35:59 +0300

    preauth

--- client/src/components/pages/AccountPage.jsx
+++ client/src/components/pages/AccountPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import MessageItem from '../ui/MessageItem';
 
 export default function AccountPage() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    // Загрузить только собственные
+  }, []);
   return (
-    <div>AccountPage</div>
+    <Row>
+      {messages.map((message) => (
+        <Col xs={12} key={message.id}>
+          <MessageItem message={message} />
+        </Col>
+      ))}
+    </Row>
   );
 }
